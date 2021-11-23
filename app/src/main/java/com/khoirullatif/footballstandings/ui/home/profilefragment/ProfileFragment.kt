5a3169ecd2926eb49d33dfeb3dc634889ff74145
Chairullatif Aji Sadewa <khoirullatif337@gmail.com>
2021-11-23 22:20:26 +0700

    add photo profile

--- app/src/main/java/com/khoirullatif/footballstandings/ui/home/profilefragment/ProfileFragment.kt
+++ app/src/main/java/com/khoirullatif/footballstandings/ui/home/profilefragment/ProfileFragment.kt
@@ -5,17 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.khoirullatif.footballstandings.R
+import com.khoirullatif.footballstandings.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
+
+    private lateinit var binding: FragmentProfileBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Glide.with(view.context)
+            .load(R.drawable.chairullatif)
+            .into(binding.circleImageView)
+    }
 }
